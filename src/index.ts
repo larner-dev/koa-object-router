167b41fc6cdd88324dbc0ec775764002cea5dd7c
Aaron Larner <alarner@users.noreply.github.com>
2023-03-27 19:07:11 -0700

    Expose loadRoutes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { router } from "./router";
 export { handleRequest } from "./handleRequest";
 export { esbuildPluginMakeRouterObject } from "./esbuildPluginMakeRouterObject";
 export { Router, RouteHandler } from "./types";
+export { loadRoutes } from "./loadRoutes";
