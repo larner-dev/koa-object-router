5aaf83fcd4b58c5c42e482e4135f0e778cb3ab05
Aaron Larner <835542+alarner@users.noreply.github.com>
2024-07-09 18:38:04 -0700

    Export RouteHandlerResult

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { router } from "./router";
 export { handleRequest } from "./handleRequest";
 export { esbuildPluginMakeRouterObject } from "./esbuildPluginMakeRouterObject";
-export type { Router, RouteHandler } from "./types";
+export type { Router, RouteHandler, RouteHandlerResult } from "./types";
 export { loadRoutes } from "./loadRoutes";
 export { createMockContext } from "./createMockContext";
