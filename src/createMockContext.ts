33ff2a2305566e8a83203c976f70d2c9feddcd09
Aaron Larner <835542+alarner@users.noreply.github.com>
2024-09-06 08:51:18 -0700

    Fix customRequestProperties

--- src/createMockContext.ts
+++ src/createMockContext.ts
@@ -106,16 +106,14 @@ export function createMockContext<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   res.set = undefined as any;
 
-  const context = {
-    ...app.createContext(req, res),
-    ...extensions,
-  } as unknown as MockContext & CustomProperties;
+  const context = app.createContext(req, res) as MockContext & CustomProperties;
+  Object.assign(context, extensions);
   //   context.cookies = createMockCookies(cookies);
 
   // ctx.request.body is a common enough custom property for middleware to add that it's handy to just support it by default
   context.request.body = requestBody;
   context.request.rawBody = rawBody;
-  context.request = { ...context.request, ...customRequestProperties };
+  Object.assign(context.request, customRequestProperties);
 
   return context as Context;
 }
